7f67098811e76b8987b697ed70333d82ec4468ec
Fahad Ali <141234330+md-fahad-ali@users.noreply.github.com>
2024-07-27 15:55:37 +0600

    update:tada

--- backend/testdb.js
+++ backend/testdb.js
@@ -1,12 +1,44 @@
-const CryptoJS = require("crypto-js");
+const connectDb = require("./utils/db");
+const User = require("./models/users");
+const Chats = require("./models/chat");
 
-jsonStr = '{"something":"else"}';
-var encrypted = CryptoJS.AES.encrypt(jsonStr, "youngunicornsrunfree");
-var decrypted = CryptoJS.AES.decrypt(
-  "U2FsdGVkX18+mPBEyxfQp1PRe5bUvzO864PmKKkHMzSyAXr2Qe9bJKAbO/ZL/S8o",
-  "youngunicornsrunfree"
-);
-console.log(decrypted.toString(CryptoJS.enc.Utf8));
+(async () => {
+  await connectDb();
+  // const user = await User.findOne({ username: "fahad288" });
+  // const chats = await Chats.find({
+  //   $or: [{ fromUsername: user.username }, { toUsername: user.username }],
+  // });
 
-var encrypted = CryptoJS.AES.encrypt(jsonStr, "youngunicornsrunfree");
-console.log("We send this: " + encrypted.toString());
+  const messages = await User.aggregate([
+    {
+      $lookup: {
+        from: "messages",
+        localField: "username",
+        foreignField: "fromUsername",
+        as: "messages",
+      },
+    },
+    {
+      $addFields: {
+        latest_message: {
+          $arrayElemAt: ["$messages", -1],
+        },
+      },
+    },
+    {
+      $sort: {
+        "latest_message.timestamp": -1,
+      },
+    },
+    {
+      $project: {
+        password: 0,
+        socket_id: 0,
+        __v: 0,
+        messages: 0,
+      },
+    },
+  ]);
+
+  console.log(messages);
+})();
